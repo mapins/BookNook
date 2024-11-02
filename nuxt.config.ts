67e88436f07afacadbea0c36f2a5cc9f17ddd57e
mapins <mariocha82004@gmail.com>
2024-11-02 21:14:00 +0100

    feat: starting the project

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData:
+            '@use "sass:map"; @import "assets/styles/mixin.scss";',
+        },
+      },
+    },
+  },
+});
