6d58cbaa553b64d1997ffdf4765d56bb1dd74393
mapins <mariocha82004@gmail.com>
2024-11-03 20:16:34 +0100

    feat(nuxt):imported css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
+import { cssImports, svgImports } from "./config";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  alias: {
+    "@": "/src",
+  },
+  css: cssImports,
   vite: {
     css: {
       preprocessorOptions: {
