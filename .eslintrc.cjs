a32b7341774081aac15a0d6e7f5151ae36df8491
mapins <mariocha82004@gmail.com>
2024-11-07 16:49:49 +0100

    chore(config): updated typescript units

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,7 +10,6 @@ module.exports = {
     es2021: true,
   },
   extends: ['standard', 'prettier', '@vue/eslint-config-typescript'],
-  plugins: ['prettier', 'vue'],
   overrides: [
     {
       env: {
@@ -21,44 +20,44 @@ module.exports = {
         sourceType: 'script',
       },
     },
-    {
-      files: ['*.vue'],
-      parser: 'vue-eslint-parser',
-      rules: {
-        'vue/html-indent': RULES.ERROR,
-        'vue/multiline-html-element-content-newline': RULES.ERROR,
-        'vue/first-attribute-linebreak': RULES.OFF,
-        'vue/require-default-prop': RULES.OFF,
-        'vue/singleline-html-element-content-newline': 0,
-        'vue/component-name-in-template-casing': [RULES.ERROR, 'PascalCase'],
-        'vue/html-quotes': [RULES.ERROR, 'double', { avoidEscape: true }],
-        'vue/mustache-interpolation-spacing': [RULES.ERROR, 'always'],
-        'vue/no-multi-spaces': [
-          RULES.ERROR,
-          {
-            ignoreProperties: false,
-          },
-        ],
-        'vue/no-spaces-around-equal-signs-in-attribute': [RULES.ERROR],
-        'vue/v-on-style': [RULES.ERROR, 'shorthand'],
-        'vue/v-bind-style': [RULES.ERROR, 'shorthand'],
-        'vue/multi-word-component-names': RULES.OFF,
-        'vue/no-setup-props-destructure': RULES.OFF,
-        'vue/no-v-text-v-html-on-component': RULES.OFF,
-      },
-    },
   ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  plugins: ['vue', 'prettier'],
   rules: {
-    'prettier/prettier': RULES.ERROR,
-    'arrow-spacing': RULES.ERROR,
-    'no-unused-vars': [RULES.ERROR, { args: 'all' }],
+    'no-undef': RULES.OFF,
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'vue/first-attribute-linebreak': RULES.OFF,
+    'no-unused-vars': [RULES.OFF],
+    'arrow-spacing': [RULES.ERROR],
     'object-curly-spacing': [RULES.ERROR, 'always'],
     'array-callback-return': [RULES.OFF, { checkForEach: true }],
+    'vue/require-default-prop': RULES.OFF,
+    'vue/singleline-html-element-content-newline': 0,
+    'vue/component-name-in-template-casing': [RULES.ERROR, 'PascalCase'],
+    'vue/html-quotes': [RULES.ERROR, 'double', { avoidEscape: true }],
+    'vue/mustache-interpolation-spacing': [RULES.ERROR, 'always'],
+    'vue/no-multi-spaces': [
+      RULES.ERROR,
+      {
+        ignoreProperties: false,
+      },
+    ],
+    'vue/no-spaces-around-equal-signs-in-attribute': [RULES.ERROR],
+    'vue/v-on-style': [RULES.ERROR, 'shorthand'],
+    'vue/v-bind-style': [RULES.ERROR, 'shorthand'],
+    'vue/multi-word-component-names': RULES.OFF,
     'no-return-assign': RULES.OFF,
     'no-mixed-operators': RULES.OFF,
+    'vue/no-setup-props-destructure': RULES.OFF,
+    'vue/no-v-text-v-html-on-component': RULES.OFF,
   },
 }
