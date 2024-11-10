d5f7c8e89bff1271b63a00e5d99527d80d519418
mapins <mariocha82004@gmail.com>
2024-11-10 21:15:34 +0100

    chore(interfaces): removed unused props

--- interfaces/book.ts
+++ interfaces/book.ts
@@ -3,12 +3,10 @@ export interface Book {
   title: string
   author: string
   editorial: string | null
-  isbn: string | null
   description: string | null
   coverpage: string | null
   pagecount: number
   publicationdate: string // Date
   averagerating: number | null
   totalratings: number | null
-  category_id: number
 }
