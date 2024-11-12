75f9ad297c8d96a446a66f4d73a996da762f57a7
mapins <mariocha82004@gmail.com>
2024-11-13 00:02:30 +0100

    feat(pages): implemented Carousel and Hero component for the principal page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
+<script setup lang="ts">
+import Carousel from '@/components/Carousel.vue'
+import Hero from '@/components/Hero.vue'
+</script>
 <template>
-  <div>
-    <h1>Welcome to the homepage</h1>
-    <AppAlert> This is an auto-imported component </AppAlert>
-  </div>
+  <Hero
+    title="Descubre, los mejores libros"
+    subtitle="Todo tipos de libros a tu alcance, averigua sobre ellos"
+    button-text="Crea tu cuenta"
+    button-link="/register"
+  />
+  <Carousel />
 </template>
