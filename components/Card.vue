b2a0aec47c7ee274396f3e9af471f4095f11614a
mapins <mariocha82004@gmail.com>
2024-11-12 23:59:49 +0100

    chore(components): fixed labels and styles

--- components/Card.vue
+++ components/Card.vue
@@ -1,28 +1,34 @@
 <script lang="ts" setup>
-import { NuxtLink } from '#build/components'
-
-defineProps({
-  book_id: String,
-  coverpage: String,
-  title: String,
-  rating: Number,
-})
+defineProps<{
+  book_id: String
+  coverpage: String
+  title: String
+}>()
 </script>
 
 <template>
   <div class="card">
     <NuxtLink :to="{ path: `/book/${book_id}` }" class="card__link">
-      <div class="card__img">
-        <NuxtImg :src="coverpage" />
-      </div>
-
-      <h2 class="card__name">{{ title }}</h2>
+      <NuxtImg :src="coverpage" class="card__img" />
+      <h2 class="card__title">{{ title }}</h2>
 
       <div class="card__rating">
-        <div class="card__stars">
-          <NuxtImg src="@/assets/images/stars.png" />
-        </div>
+        <div class="card__stars"></div>
       </div>
     </NuxtLink>
   </div>
 </template>
+<style scoped lang="scss">
+.card {
+  color: var(--c-white);
+  &__img {
+    height: 15rem;
+    object-fit: contain;
+    box-shadow: 0rem 2rem 5rem rgba(0, 0, 0, 0.35);
+  }
+  &__title {
+    font-size: 1rem;
+    text-align: center;
+  }
+}
+</style>
