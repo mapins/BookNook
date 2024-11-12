d359f2777b8f0f977648c662c34de94d45fe5e62
mapins <mariocha82004@gmail.com>
2024-11-12 23:57:17 +0100

    chore(config): added nuxt image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import { cssImports } from './config'
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt'],
+  modules: ['@pinia/nuxt', '@nuxt/image'],
 
   css: cssImports,
   vite: {
