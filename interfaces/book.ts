fc2c48bc8efe97bf2d0ddcb2b90469212940576f
mapins <mariocha82004@gmail.com>
2024-11-13 22:34:55 +0100

    chore(interfaces): fixed book interface

--- interfaces/book.ts
+++ interfaces/book.ts
@@ -1,12 +1,12 @@
 export interface Book {
   book_id: number
   title: string
-  author: string
-  editorial: string | null
-  description: string | null
-  coverpage: string | null
+  author: { name: string }[]
+  editorial: string
+  description: string
+  coverpage: string
   pagecount: number
-  publicationdate: string // Date
-  averagerating: number | null
-  totalratings: number | null
+  publicationdate: number
+  averagerating: number
+  totalratings: number
 }
