ffbdad7f1b2d42a5137e180d72a5235fa5eaf913
mapins <mariocha82004@gmail.com>
2024-11-18 13:51:27 +0100

    feat(pages): implemented the search bar in the page

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
 import Carousel from '@/components/Carousel.vue'
 import Hero from '@/components/Hero.vue'
+import { useElementStore } from '~/stores/search'
+
+const { isShowing } = storeToRefs(useElementStore())
 </script>
 <template>
+  <transition name="slidedown">
+    <SearchForm v-if="isShowing" />
+  </transition>
   <Hero
     title="Descubre, los mejores libros"
     subtitle="Todo tipos de libros a tu alcance, averigua sobre ellos"
