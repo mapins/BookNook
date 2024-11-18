24b8392288b8991ae488affbf5c08cde807490e8
mapins <mariocha82004@gmail.com>
2024-11-18 17:39:01 +0100

    feat(pages): implemented the search bar

--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,35 @@
 import Carousel from '@/components/Carousel.vue'
 import Hero from '@/components/Hero.vue'
 import { useElementStore } from '~/stores/search'
+import { getBooks } from '~/services/bookService'
 
 const { isShowing } = storeToRefs(useElementStore())
+
+const books = ref([])
+
+onMounted(async () => {
+  try {
+    const fetchedBooks = await getBooks()
+    books.value = fetchedBooks || []
+    console.log(books.value)
+  } catch (error) {
+    console.error('Error al obtener los libros:', error)
+  }
+})
 </script>
 <template>
-  <transition name="slidedown">
-    <SearchForm v-if="isShowing" />
-  </transition>
-  <Hero
-    title="Descubre, los mejores libros"
-    subtitle="Todo tipos de libros a tu alcance, averigua sobre ellos"
-    button-text="Crea tu cuenta"
-    button-link="/register"
-  />
-  <Carousel />
+  <div class="home" v-if="!isShowing">
+    <Hero
+      title="Descubre, los mejores libros"
+      subtitle="Todo tipos de libros a tu alcance, averigua sobre ellos"
+      button-text="Crea tu cuenta"
+      button-link="/register"
+    />
+    <Carousel :books />
+  </div>
+  <div v-else class="listing">
+    <transition name="slidedown">
+      <SearchForm />
+    </transition>
+  </div>
 </template>
