49174b9578846ec6265c24d87710eb00c9e50d46
mapins <mariocha82004@gmail.com>
2024-11-20 16:33:54 +0100

    feat(components): created component for listing books

---
+++ components/Listing.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineProps<{
+  books: object
+}>()
+</script>
+<template>
+  <div class="listing">
+    <ul>
+      <li v-for="(book, index) in books" :key="index">
+        <Card :coverpage="book.coverpage" :book_id="book.book_id" :title="book.title" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.listing ul {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  overflow: scroll;
+  height: 80vh;
+  padding: 1rem;
+}
+
+.listing li {
+  flex: 0 1 calc(25% - 1rem);
+  list-style: none;
+  display: flex;
+}
+</style>
