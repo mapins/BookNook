04a4b2a21c1fc9f92dc5e502e2f7a1d5681a4941
mapins <mariocha82004@gmail.com>
2024-11-20 16:42:39 +0100

    feat(stores): saving the data in localStorage

--- stores/auth.ts
+++ stores/auth.ts
@@ -1,29 +1,41 @@
 import type { UserData } from '~/interfaces/user'
 
 export const useAuthStore = defineStore('auth', () => {
-  const user = ref()
+  const user = ref<UserData | null>(null)
   const isLoggedIn = ref(false)
   const authToken = useCookie('authToken')
-  const userId = ref()
-
-  onMounted(() => {
-    if (authToken.value) {
-      isLoggedIn.value = true
-    } else {
-      isLoggedIn.value = false
-    }
-  })
+  const userId = ref<number | null>(null)
 
   function login(userData: UserData) {
     user.value = userData
     isLoggedIn.value = true
     userId.value = user.value?.user_id || null
+    localStorage.setItem('userId', userId.value?.toString() || '')
+    localStorage.setItem('isLoggedIn', 'true')
   }
 
   function logout() {
     user.value = null
     isLoggedIn.value = false
+    userId.value = null
+    localStorage.removeItem('userId')
+    localStorage.removeItem('isLoggedIn')
   }
+
+  onMounted(() => {
+    const storedUserId = localStorage.getItem('userId')
+    const storedIsLoggedIn = localStorage.getItem('isLoggedIn')
+    console.log(storedUserId)
+
+    if (authToken.value && storedIsLoggedIn === 'true' && storedUserId) {
+      isLoggedIn.value = true
+      userId.value = parseInt(storedUserId)
+      console.log(userId.value, isLoggedIn.value)
+    } else {
+      isLoggedIn.value = false
+      userId.value = null
+    }
+  })
 
   return {
     user,
