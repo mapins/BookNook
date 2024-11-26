f8c83996edb01510cac0aff875696e6960e69615
mapins <mariocha82004@gmail.com>
2024-11-26 17:31:32 +0100

    feat(services):category search implementation test

--- services/bookService.ts
+++ services/bookService.ts
@@ -43,3 +43,24 @@ export const getBookById = async (bookId: number) => {
     throw error
   }
 }
+
+export const getBookByCategory = async (categoryId: number) => {
+  /* console.log('SEMETI')
+  const cacheStore = useCacheStore()
+
+  const cachedData = cacheStore.getCache(`book-${bookId}`)
+  if (cachedData) {
+    console.log('Datos obtenidos del cache')
+    return cachedData
+  } */
+  try {
+    const response = await fetch(`${BASE_URL}/category/${categoryId}`)
+    if (!response.ok) throw new Error('Error al obtener el libro')
+    const data = await response.json()
+    /*     cacheStore.setCache(`book-${bookId}`, data)
+     */ return data
+  } catch (error) {
+    console.error(`Error al obtener los libros con categoria ${categoryId}:`, error)
+    throw error
+  }
+}
