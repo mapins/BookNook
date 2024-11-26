b8329f3f9321dda3763e05ab92ca88c84bc72147
mapins <mariocha82004@gmail.com>
2024-11-26 20:47:20 +0100

    chore(components): removed unused things and created svg for cross

--- components/global/search/SearchForm.vue
+++ components/global/search/SearchForm.vue
@@ -2,8 +2,7 @@
 import Listing from '~/components/Listing.vue'
 import { useFilterBooks } from '~/composables/useFilterBooks'
 import { getBooks } from '~/services/bookService'
-import { useElementStore } from '@/stores/search'
-const { handleElement } = useElementStore()
+import Cross from '~/components/svg/Cross.vue'
 const { filteredBooks, searchBook } = useFilterBooks()
 
 const books = ref([])
@@ -37,22 +36,7 @@ watch(searchTerm, (newTerm) => {
       />
 
       <button type="button" aria-label="Close" @click="handleElement">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="15"
-          height="15"
-          viewBox="0 0 15 15"
-        >
-          <g
-            fill="none"
-            stroke="#fff"
-            stroke-linecap="round"
-            stroke-miterlimit="10"
-            stroke-width="1.5"
-          >
-            <path d="M.75.75l13.5 13.5M14.25.75L.75 14.25" />
-          </g>
-        </svg>
+        <Cross />
       </button>
     </div>
 
