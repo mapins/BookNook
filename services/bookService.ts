0857e0c9a5c1ee6b294ce42aee2099effa2b7f9b
mapins <mariocha82004@gmail.com>
2024-12-03 18:25:48 +0100

    feat(services): implemented to search for books by slug and also pull out the categories of these.

--- services/bookService.ts
+++ services/bookService.ts
@@ -43,22 +43,23 @@ export const getBookById = async (bookId: number) => {
     throw error
   }
 }
-export const getBookByTitle = async (bookTitle: string) => {
+export const getBookBySlug = async (bookSlug: string) => {
+  console.log(bookSlug)
   const cacheStore = useCacheStore()
 
-  const cachedData = cacheStore.getCache(`book-${bookTitle}`)
+  const cachedData = cacheStore.getCache(`book-${bookSlug}`)
   if (cachedData) {
     console.log('Datos obtenidos del cache')
     return cachedData
   }
   try {
-    const response = await fetch(`${BASE_URL}/title/${bookTitle}`)
+    const response = await fetch(`${BASE_URL}/slug/${bookSlug}`)
     if (!response.ok) throw new Error('Error al obtener el libro')
     const data = await response.json()
-    cacheStore.setCache(`book-${bookTitle}`, data)
+    cacheStore.setCache(`book-${bookSlug}`, data)
     return data
   } catch (error) {
-    console.error(`Error al obtener el libro con titulo ${bookTitle}:`, error)
+    console.error(`Error al obtener el libro con titulo ${bookSlug}:`, error)
     throw error
   }
 }
@@ -83,6 +84,26 @@ export const getBooksByCategories = async (categoryIds: number[]) => {
     return data
   } catch (error) {
     console.error(`Error al obtener los libros con categoria ${categoryIds}:`, error)
+    throw error
+  }
+}
+export const getBookCategories = async (bookId: number) => {
+  const cacheStore = useCacheStore()
+
+  const cachedData = cacheStore.getCache(`books-category-${bookId}`)
+  if (cachedData) {
+    console.log('Datos obtenidos del cache')
+    return cachedData
+  }
+  try {
+    const response = await fetch(`${BASE_URL}/${bookId}/categories`)
+
+    if (!response.ok) throw new Error('Error al obtener el libro')
+    const data = await response.json()
+    cacheStore.setCache(`books-category-${bookId}`, data)
+    return data
+  } catch (error) {
+    console.error(`Error al obtener las categorias con libro ID ${bookId}:`, error)
     throw error
   }
 }
