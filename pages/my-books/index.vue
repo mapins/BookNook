522bf9868bbc1b8b8f3be39e04480defbf8a7ca5
mapins <mariocha82004@gmail.com>
2024-12-05 19:23:07 +0100

    feat(pages): implemented that some pages do not have a footer and also have to be logged in.

--- pages/my-books/index.vue
+++ pages/my-books/index.vue
@@ -1,5 +1,9 @@
 <script setup>
 import ReadingStatusNav from '~/components/ReadingStatusNav.vue'
+definePageMeta({
+  layout: 'no-footer',
+  middleware: 'auth',
+})
 </script>
 <template>
   <ReadingStatusNav />
--- pages/search/index.vue
+++ pages/search/index.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
-import BookSearch from '~/components/global/search/BookSearch.vue'
+import BookSearch from '@/components/global/search/BookSearch.vue'
+definePageMeta({
+  layout: 'no-footer',
+})
 </script>
 <template>
   <transition name="slidedown">
