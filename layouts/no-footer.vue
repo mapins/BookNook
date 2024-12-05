78245a3cbeaadb0f7793514a94bb6469b8ea49ad
mapins <mariocha82004@gmail.com>
2024-12-05 19:34:15 +0100

    feat(layouts): implemented a layout for when you want some page without footer

---
+++ layouts/no-footer.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import MainNavbar from '@/components/global/navbar/MainNavbar.vue'
+</script>
+
+<template>
+  <div>
+    <MainNavbar />
+    <NuxtPage />
+  </div>
+</template>
