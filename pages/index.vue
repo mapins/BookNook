7c11f9d381e6f2efc37944237bff6d5bcd301e51
mapins <mariocha82004@gmail.com>
2024-12-10 02:43:13 +0100

    feat(pages): information removed from page indexes and incorporated into separate components

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,6 @@
 <script setup lang="ts">
-import Carousel from '@/components/Carousel.vue'
-import Hero from '@/components/Hero.vue'
-import { useElementStore } from '~/stores/search'
-import { getBooks } from '~/services/bookService'
-
-const { isShowing } = storeToRefs(useElementStore())
-
-const books = ref([])
-
-onMounted(async () => {
-  try {
-    const fetchedBooks = await getBooks()
-    books.value = fetchedBooks || []
-  } catch (error) {
-    console.error('Error al obtener los libros:', error)
-  }
-})
+import MainHome from '@/components/MainHome.vue'
 </script>
 <template>
-  <main class="main">
-    <Hero
-      title="Descubre, los mejores libros"
-      subtitle="Todo tipos de libros a tu alcance, averigua sobre ellos"
-      button-text="CREA TU CUENTA"
-      button-link="/register"
-    />
-    <Carousel :books />
-  </main>
+  <MainHome />
 </template>
