3cf229b11a395842e91d7b47fa93b324c843db13
mapins <mariocha82004@gmail.com>
2024-12-10 02:48:40 +0100

    chore(interfaces): fixed atributes

--- interfaces/user.ts
+++ interfaces/user.ts
@@ -3,8 +3,8 @@ export interface UserData {
   user_handle: string
   email_address: string
   password?: string
-  first_name: string
-  last_name: string
+  first_name?: string
+  last_name?: string
 }
 
 export interface UpdateUserData {
