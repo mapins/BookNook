e2206093c85d167d86881014220602fd3c85653f
mapins <mariocha82004@gmail.com>
2024-12-10 17:54:29 +0100

    feat(components): implemented new hero and svg for when it is loading

--- components/MainHome.vue
+++ components/MainHome.vue
@@ -1,23 +1,20 @@
 <script setup lang="ts">
 import Carousel from '@/components/Carousel.vue'
-import Hero from '@/components/Hero.vue'
+import Loading from '@/components/svg/Loading.vue'
+import HeroHome from '@/components/HeroHome.vue'
 import type { Book } from '~/interfaces'
 import type { Category } from '~/interfaces/category'
 import { getBooks, getBooksByCategories } from '~/services/bookService'
 import { getCategories } from '~/services/categoriesService'
 
 const allBooks = ref<Book[]>([])
-const currentBookIndex = ref(0)
 const categories = ref<Category[]>([])
 const booksByCategory = ref<Record<number, Book[]>>({})
+const isLoading = ref(true)
 
 onMounted(async () => {
   try {
     allBooks.value = await getBooks()
-
-    /* setInterval(() => {
-      currentBookIndex.value = (currentBookIndex.value + 1) % allBooks.value.length
-    }, 15000) */
 
     categories.value = await getCategories()
     for (const category of categories.value) {
@@ -26,38 +23,51 @@ onMounted(async () => {
     }
   } catch (error) {
     console.error('Error al obtener los libros:', error)
+  } finally {
+    isLoading.value = false
   }
 })
 </script>
 <template>
   <main class="home">
-    <!-- <Hero
-      :title="allBooks[currentBookIndex].title"
-      :authors="allBooks[currentBookIndex].author"
-      :publicationDate="allBooks[currentBookIndex].publicationdate"
-      :pageCount="allBooks[currentBookIndex].pagecount"
-      :subtitle="allBooks[currentBookIndex].description"
-      button-text="CREA TU CUENTA"
-      button-link="/register"
-    /> -->
-    <h3 class="home__title">Todos los Libros</h3>
-    <Carousel :books="allBooks" />
+    <HeroHome />
+    <div v-if="isLoading" class="home__loading">
+      <Loading />
+    </div>
+    <div v-else>
+      <h3 v-if="allBooks.length" class="home__title">Todos los Libros</h3>
+      <Carousel v-if="allBooks.length" :books="allBooks" />
 
-    <div v-for="category in categories" :key="category.category_id">
-      <h3 class="home__title">{{ category.name }}</h3>
-      <Carousel :books="booksByCategory[category.category_id]" />
+      <div v-for="category in categories" :key="category.category_id">
+        <h3 v-if="booksByCategory[category.category_id]?.length" class="home__title">
+          {{ category.name }}
+        </h3>
+        <Carousel
+          v-if="booksByCategory[category.category_id]?.length"
+          :books="booksByCategory[category.category_id]"
+        />
+      </div>
     </div>
   </main>
 </template>
 
 <style scoped lang="scss">
+@import '/assets/styles/load.css';
+
 .home {
   background-color: var(--c-graphite);
+  animation: fadeInUp 1.5s ease-out;
   &__title {
     background-color: var(--c-graphite);
     color: var(--c-white);
     margin: 0 5rem;
     padding: 3rem 0 0 0;
+  }
+  &__loading {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 20rem;
   }
 }
 </style>
