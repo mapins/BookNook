c5eaf2c7669757f66fb04d598425431979d20242
mapins <mariocha82004@gmail.com>
2024-12-10 19:40:15 +0100

    feat(config):implemented page title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   modules: ['@pinia/nuxt', '@nuxt/image'],
-
+  app: {
+    head: {
+      title: 'BookNook',
+    },
+  },
   css: cssImports,
   vite: {
     css: {
