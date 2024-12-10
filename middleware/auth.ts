3d60aac36eb9140725da438e422cf9b4ec037d52
mapins <mariocha82004@gmail.com>
2024-12-10 17:43:13 +0100

    chore(middleware): fixed middleware for auth

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -4,11 +4,11 @@ export default defineNuxtRouteMiddleware((to, from) => {
   const authStore = useAuthStore()
   const { user } = storeToRefs(authStore)
 
-  if (user && (to.path === '/login' || to.path === '/register')) {
+  if (user.value && (to.path === '/login' || to.path === '/register')) {
     return abortNavigation()
   }
 
-  if (!user && to.path !== '/login' && to.path !== '/register') {
+  if (!user.value && to.path !== '/login' && to.path !== '/register') {
     return navigateTo('/login')
   }
 })
